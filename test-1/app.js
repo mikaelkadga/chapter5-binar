e230ea644fd39a481b830b8c731ef51b31f66383
mikaelkadga <mikaelkadga@gmail.com>
2022-07-11 12:50:12 +0700

    test-1-update

--- test-1/app.js
+++ test-1/app.js
@@ -1,10 +1,80 @@
-const { saveContact, question } = require("./contact");
+// const { saveContact, question } = require("./contact");
 
-const main = async () => {
-  const name = await question('name? ');
-  const email = await question('email? ');
+// const main = async () => {
+//   const name = await question('name? ');
+//   const email = await question('email? ');
 
-  saveContact(name, email)
-}
+//   saveContact(name, email)
+// }
 
-main();
+// main();
+
+
+const yargs = require("yargs");
+const { saveContact, listContact, detailContact, deleteContact } = require("./contact");
+
+yargs.command({
+  command: 'add', 
+  describe: 'add new contact',
+  builder: {
+    name: {
+      describe: 'name',
+      demandOption: true, 
+      type: 'string'
+    },
+    email: {
+      describe: 'email',
+      demandOption: false, 
+      type: 'string'
+    },
+    phoneNumber: {
+      describe: 'phoneNumber',
+      demandOption: true, 
+      type: 'string'
+    }
+  },
+  handler(argv) {
+  saveContact(argv.name, argv.email, argv.phoneNumber)
+  }
+})
+.demandCommand();
+
+yargs.command({
+  command: 'list', 
+  describe: 'list all contact',
+  handler() {
+    listContact()
+  }
+});
+
+yargs.command({
+  command: 'detail', 
+  describe: 'contact detail',
+  builder: {
+    name: {
+      describe: 'name',
+      demandOption: true, 
+      type: 'string' 
+    } 
+  },
+  handler(argv) {
+    detailContact(argv.name);
+  }
+});
+
+yargs.command({
+  command: 'delete', 
+  describe: 'delete contact',
+  builder: {
+    name: {
+      describe: 'name',
+      demandOption: true, 
+      type: 'string' 
+    } 
+  },
+  handler(argv) {
+    deleteContact(argv.name);
+  }
+});
+
+yargs.parse();
--- test-1/contact.js
+++ test-1/contact.js
@@ -1,22 +1,21 @@
-const readline = require('readline');
-const fs = require('fs');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 //directory checking
-const dirPath = './data';
-if(!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath);
-}
+// const dirPath = './data';
+// if(!fs.existsSync(dirPath)) {
+//   fs.mkdirSync(dirPath);
+// }
 
 //file checking
-const dataPath = './data/contacts.json';
-if(!fs.existsSync(dataPath)) {
-  fs.writeFileSync(dataPath, '[]', 'utf-8')
-}
+// const dataPath = './data/contacts.json';
+// if(!fs.existsSync(dataPath)) {
+//   fs.writeFileSync(dataPath, '[]', 'utf-8')
+// }
 
 //avoid callback hell
 // rl.question('name? ', (name) => {
@@ -35,28 +34,116 @@ if(!fs.existsSync(dataPath)) {
 //     })
 // });
 
-const question = (q) => {
-  return new Promise((resolve, reject) => {
-    rl.question(q, (qParam) => {
-      resolve(qParam);
-    });
-  });
-};
+// const question = (q) => {
+//   return new Promise((resolve, reject) => {
+//     rl.question(q, (qParam) => {
+//       resolve(qParam);
+//     });
+//   });
+// };
+
+
+const fs = require('fs');
+const validator = require('validator');
+
+//directory checking
+const dirPath = './data';
+if(!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+//file checking
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, '[]', 'utf-8')
+}
+
+
+//load contact
+const loadContact = () => {
+  const file = fs.readFileSync('./data/contacts.json', 'utf-8');
+  const contacts = JSON.parse(file);
+  return contacts;
+}
 
 
 //save contact to contacts.json
-const saveContact = (name, email) => {
-    const contact = {name , email};
+const saveContact = (name, email, phoneNumber) => {
+    const contact = {name , email, phoneNumber};
         
     //read contacts.json 
-    const file = fs.readFileSync('./data/contacts.json', 'utf-8');
-    const contacts = JSON.parse(file);
-    contacts.push(contact);
+    const contacts = loadContact();
+    
+
+    //duplicate checking
+    const duplicate = contacts.find(contact => contact.name == name);
+    if(duplicate) {
+      console.log('name has already taken');
+      return false;
+    }
+
+    //email checking
+    if (email) {
+      if(!validator.isEmail(email)) {
+        console.log('email is not valid');
+      return false;
+      }
+    }
+
+    //phoneNumber checking
+    if(!validator.isMobilePhone(phoneNumber, 'id-ID')) {
+      console.log('phone number is not valid');
+    return false;
+    }
+    
 
     //write contacts.json
+    contacts.push(contact);
     fs.writeFileSync('./data/contacts.json', JSON.stringify(contacts));
-    console.log('terima kasih')
-    rl.close();
+    console.log('thanks');
+    // rl.close();
+    //we don't need readline anymore
 }
 
-module.exports = {question, saveContact}
+const listContact = () => {
+  const contacts = loadContact();
+  console.log('Contact List');
+  contacts.forEach((contact, i) => {
+    console.log(`${i+1}. ${contact.name} - ${contact.phoneNumber}`)
+  })
+}
+
+const detailContact = (name) => {
+  const contacts = loadContact();
+
+  const contact = contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+
+  if(!contact) {
+    console.log(`name ${name} is not found!`);
+    return false;
+  }
+
+  console.log('Contact Detail')
+  console.log(`name: ${contact.name}`);
+  console.log(`phone number: ${contact.phoneNumber}`);
+  if(contact.email) {
+    console.log(`email: ${contact.email}`);
+  }
+}
+
+const deleteContact = (name) => {
+  const contacts = loadContact();
+
+  const newContacts = contacts.filter(contact => contact.name.toLowerCase() !== name.toLowerCase())
+
+  if(contacts.length === newContacts.length) {
+    console.log(`name ${name} is not found!`);
+    return false;
+  }
+
+  fs.writeFileSync('./data/contacts.json', JSON.stringify(newContacts));
+  console.log(`${name}, has been deleted`);
+
+}
+
+module.exports = {saveContact, listContact, detailContact, deleteContact}
